da95771659c5d66b61609610bb1cccbc62b40309
Wang Siyuan <wsy0227@sjtu.edu.cn>
2025-07-30 07:32:41 +0000

    update needle tools for sampling params

--- experiments/needle_in_a_haystack/needle_tools.py
+++ experiments/needle_in_a_haystack/needle_tools.py
@@ -240,7 +240,16 @@ class LLMNeedleHaystackTester:
                 # max_model_len=context_lengths_max,
                 **kwargs,
             )
-            self.generation_config = SamplingParams(temperature=0.6, max_tokens=8192 if os.getenv("NO_THINK", "0") != "1" else 64)
+            if os.getenv("NO_THINK", "0") != "1":
+                self.generation_config = SamplingParams(
+                    temperature=0.6,
+                    max_tokens=8192,
+                )
+            else:
+                self.generation_config = SamplingParams(
+                    temperature=0,
+                    max_tokens=64,
+                )
         else:
             if self.config.attn_type == "hf":
                 self.model = AutoModelForCausalLM.from_pretrained(
